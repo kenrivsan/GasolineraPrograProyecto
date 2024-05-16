639f02ec1c49be58ba45cbc21c605342dc08699a
kenri <kennethriv@umes.edu.gt>
2024-05-15 22:54:08 -0600

    CodigoDeDatos

--- GasolineraPrograProyecto/Ingreso.cs
+++ GasolineraPrograProyecto/Ingreso.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace GasolineraPrograProyecto
-{
+{   
+    
     public partial class Ingreso : Form
     {
+        List<clsDatos> datos = new List<clsDatos>();
         public Ingreso()
         {
             InitializeComponent();
@@ -20,6 +23,32 @@ namespace GasolineraPrograProyecto
         private void btnPanel_Click(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            clsDatos dato = new clsDatos();
+            dato.Nit = txtNit.Text;
+            dato.Nombre = txtNombre.Text;
+            dato.Fecha = dateTimeHora.Value;
+            dato.Tipogasolina = cmbGasolina.SelectedIndex.ToString();
+            if (rdbprepago.Checked)
+            {
+                dato.Tipogasolina = "Prepago";
+            }
+            else if (rdbprepagolleno.Checked)
+            {
+                dato.Tipogasolina = "PrepagoLleno";
+            }
+            datos.Add(dato);
+            GuardadoDatos();
+
+        }
+        void GuardadoDatos()
+        {
+            string DatosGuardar = JsonConvert.SerializeObject(datos);
+            string Archivo = "Datos.json";
+            System.IO.File.WriteAllText(Archivo, DatosGuardar);
         }
     }
 }
