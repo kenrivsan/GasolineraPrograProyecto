ed3755f721b396b9c1672a35eb2efc4e44ba625e
kenri <kennethriv@umes.edu.gt>
2024-05-27 10:40:04 -0600

    beta

--- GasolineraPrograProyecto/Ingreso.Designer.cs
+++ GasolineraPrograProyecto/Ingreso.Designer.cs
@@ -143,7 +143,7 @@
             // 
             // btnPanel
             // 
-            this.btnPanel.Location = new System.Drawing.Point(395, 315);
+            this.btnPanel.Location = new System.Drawing.Point(395, 12);
             this.btnPanel.Name = "btnPanel";
             this.btnPanel.Size = new System.Drawing.Size(75, 39);
             this.btnPanel.TabIndex = 23;
